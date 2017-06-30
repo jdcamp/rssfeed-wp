aa1ddfcd25ff9265317cc3467e7284b247f321a7
jdc <jdcxxx@gmail.com>
2017-06-30 15:31:24 -0700

    ignore php_error.log file, addcomment to rssfeeder.php

--- wp-content/plugins/rssfeeder/rssfeeder.php
+++ wp-content/plugins/rssfeeder/rssfeeder.php
@@ -1,25 +1,32 @@
 <?php
-/*
-Plugin Name: RSS Autoblog
-Description: Simple auto blogger. Takes in rss feeds and posts
-Version: 0.9
-Author: Jake C & Jayeson K
+/**
+* @package RSS Autoblog
+*Plugin Name: RSS Autoblog
+*Description: Simple auto blogger. Takes in rss feeds and posts
+*Version: 0.9
+*Author: Jake C & Jayeson K
 */
 use PicoFeed\Reader\Reader;
+
 require 'vendor/autoload.php';
+
 require_once 'feed-create.php';
+
 require_once 'init.php';
+
 require_once 'feed-update.php';
+
 require_once 'feed-list.php';
-// add_action('admin_menu', 'add_feed_menu_page');
-//on install runs table_install()
-//Installs wp_feeder database
+
+/**
+* Adds feeder table to DB.
+*/
 function rssfeeder_install()
 {
-  global $wpdb;
-  $table_name = $wpdb->prefix . "feeder";
-  $charset_collate = $wpdb->get_charset_collate();
-  $sql = "CREATE TABLE $table_name (
+    global $wpdb;
+    $table_name = $wpdb->prefix . "feeder";
+    $charset_collate = $wpdb->get_charset_collate();
+    $sql = "CREATE TABLE $table_name (
       id int(11) NOT NULL AUTO_INCREMENT,
       title tinytext NOT NULL,
       feed_url varchar(255) DEFAULT '' NOT NULL,
@@ -27,15 +34,16 @@ function rssfeeder_install()
       category varchar(255) DEFAULT '' NOT NULL,
           PRIMARY KEY (id)
         ) $charset_collate; ";
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-  dbDelta($sql);
-  //Register category
-  wp_create_category('External Source');
-}
-register_activation_hook(__FILE__, 'rssfeeder_install');
-add_action('init', 'rssfeeder_install');
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-//checks if feed is already in the database
+    dbDelta($sql);
+}
+
+/**
+* Checks if feed is already in the database
+* @param string $url rss url
+* @return boolean
+*/
 function is_unique_feed($url)
 {
     global $wpdb;
@@ -46,131 +54,161 @@ function is_unique_feed($url)
         return false;
     }
 }
-//add feed menu item in the admin view
-function add_rss_post_page( )
+
+/**
+* add feed menu item in the admin view
+*/
+function add_rss_post_page()
 {
-    add_posts_page( 'rss feeder', 'rss feeder', 'manage_options', 'rss-feeder', 'rss_form' );
-    add_posts_page( 'feeds', 'feeds', 'manage_options', 'feeds-list', 'feeder_feeder_list', 'feeder_feeder_update', 'feeder_feeder_create' );
+    add_posts_page('rss feeder', 'rss feeder', 'manage_options', 'rss-feeder', 'rss_form');
+    add_posts_page('feeds', 'feeds', 'manage_options', 'feeds-list', 'feeder_feeder_list', 'feeder_feeder_update', 'feeder_feeder_create');
 }
-//returns true if id matched guid in database. prevents duplicate posts
-function is_unique_post( $id, $title )
+
+
+/**
+* checks if post exists in DB by matching the guid or title
+* @param string $id id of the rss article
+* @param string $title title of the rss article
+* @return boolean
+*/
+function is_unique_post($id, $title)
 {
     global $wpdb;
-    $id   = 'http://' . $id;
-    $table   = $wpdb->prefix . 'posts';
-    $query   = $wpdb->prepare( 'SELECT COUNT(*) FROM '.$table.' WHERE guid = "%s" OR post_title = "%s" LIMIT 1;', $id, $title );
-    $results = $wpdb->get_var( $query );
-    if ( $results > 0 ) {
+    $id = 'http://' . $id;
+    $table = $wpdb->prefix . 'posts';
+    $query = $wpdb->prepare('SELECT COUNT(*) FROM ' . $table . ' WHERE guid = "%s" OR post_title = "%s" LIMIT 1;', $id, $title);
+    $results = $wpdb->get_var($query);
+    if ($results > 0) {
         return false;
     } else {
         return true;
     }
 }
-//Checks a string if it contains any of the keywords. All keywords must match
-function check_key_words( $sentence, $keywords )
+
+/**
+* Checks if a keyword is in a string
+* @param string $sentence
+* @param array string $title title of the rss article
+* @param array $keywords {
+*       @type string $value,
+*       @type string $value,
+*       ...
+*     }
+* @return boolean
+*/
+function check_key_words($sentence, $keywords)
 {
-    if ( empty( ( array ) $keywords ) || $keywords == NULL ) {
+    if (empty(( array )$keywords) || $keywords == null) {
         return true;
     }
-    foreach ( (array) $keywords as $keyword ) {
-        $keyword = trim( $keyword );
-        if ( stripos( $sentence, $keyword ) === false ) {
+
+    foreach ((array)$keywords as $keyword) {
+        $keyword = trim($keyword);
+        if (stripos($sentence, $keyword) === false) {
             return false;
         }
     }
+
     return true;
 }
-//trys to fetch results from PicoFeed and returns true if it can get items from feed
-function is_valid_rss_url( $url )
+
+/**
+* Checks if feed is valid by trying to parse it using Reader
+* @param string $url rss url
+* @return boolean
+*/
+function is_valid_rss_url($url)
 {
     try {
-        $reader   = new Reader;
-        $resource = $reader->download( $url );
-
-        $parser = $reader->getParser( $resource->getUrl(), $resource->getContent(), $resource->getEncoding() );
-
+        $reader = new Reader;
+        $resource = $reader->download($url);
+        $parser = $reader->getParser($resource->getUrl(), $resource->getContent(), $resource->getEncoding());
         $feed = $parser->execute();
         $test = $feed->getItems();
         return true;
-    }
-    catch ( Exception $e ) {
+    } catch (Exception $e) {
         return false;
     }
 }
-//add custom fields
-function my_add_custom_fields( $post_id, $source )
+
+// add custom fields
+
+function my_add_custom_fields($post_id, $source)
 {
-    if ( $_POST[ 'post_type' ] == 'post' ) {
-        add_post_meta( $post_id, 'source', $source, true );
+    if ($_POST['post_type'] == 'post') {
+        add_post_meta($post_id, 'source', $source, true);
     }
 }
-//Gets rss feeds from DB and publishes new posts from the rss feeds
-function get_posts_from_feed( )
+
+/**
+* Grabs feeds from the DB and uses Reader to post articles. Called by cron
+*/
+function get_posts_from_feed()
 {
     global $wpdb;
-    //get table holding rss feed
     $table = $wpdb->prefix . 'feeder';
-    $result = $wpdb->get_results( "SELECT * FROM $table" );
-    foreach ( $result as $queried_feed ) {
-        $my_feed  = $queried_feed->feed_url;
+    $result = $wpdb->get_results("SELECT * FROM $table");
+    foreach ($result as $queried_feed) {
+        $my_feed = $queried_feed->feed_url;
         $keywords = $queried_feed->keywords;
         $category = $queried_feed->category;
         $category = get_term_by('name', $category, 'category');
-        //if there are keywords sets to an array
-        if ( $keywords ) {
-            $keywords = explode( ',', $keywords );
+        if ($keywords) { // if there are keywords sets to an array
+            $keywords = explode(',', $keywords);
         }
-        $reader   = new Reader;
-        $resource = $reader->download( $my_feed );
-        $parser   = $reader->getParser( $resource->getUrl(), $resource->getContent(), $resource->getEncoding() );
-        $feed     = $parser->execute();
-        $items    = $feed->getItems();
-        foreach ( $items as $item ) {
+
+        $reader = new Reader;
+        $resource = $reader->download($my_feed);
+        $parser = $reader->getParser($resource->getUrl(), $resource->getContent(), $resource->getEncoding());
+        $feed = $parser->execute();
+        $items = $feed->getItems();
+        foreach ($items as $item) {
             $title = $item->getTitle();
             $id = $item->getId();
             $author = $item->getAuthor();
-
-            if (is_unique_post($id, $title)) {
+            if (is_unique_post($id, $title)) { //checks for duplicates
                 $body = $item->getContent();
-                if (check_key_words($body, $keywords) || check_key_words($title, $keywords)) {
+                if (check_key_words($body, $keywords) || check_key_words($title, $keywords)) { //checks for the keywords
                     $author = $item->getAuthor();
-                    $user_id = get_user_by( 'login', $author );
-                    if ( !$user_id ) {
-                    	$random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
-                    	$user_id = wp_create_user( $author, $random_password);
+                    $user_id = get_user_by('login', $author);
+                    if (!$user_id) { //creates authors if author does not exist
+                        $random_password = wp_generate_password($length = 12, $include_standard_special_chars = false);
+                        $user_id = wp_create_user($author, $random_password);
                     } else {
-                    	$user_id = get_user_by('login', $author)->get('id');
+                        $user_id = get_user_by('login', $author)->get('ID');
                     }
+
                     $url = $item->getUrl();
                     $body = $item->getContent();
                     $tags = implode(', ', $item->getCategories());
-                    $body       = wp_trim_words($body, 128) .
-                                       '<br>
+                    $body = wp_trim_words($body, 128) . '<br />
                                         <p class="feed-link">
                                           <a href="' . $url . '">
                                             <button class="btn btn-single">Read Original Article Here</button>
                                             </a>
                                         </p>
-                                        <br>';
+                                        <br />';
                     $date = $item->getPublishedDate();
                     $date = date_format($date, 'Y-m-d H:i:s');
                     $trimmed = parse_url($url);
                     $source = str_replace('www.', "", $trimmed['host']);
                     $args = array(
-                     'post_author' => $user_id,
-                     'post_content' => $body,
-                     'post_title' => $title,
-                     'post_status' => "publish",
-                     'post_type' => "post",
-                     'guid' => $id,
-                     'post_category' => array($category->term_id),
-                     'meta_input' => array(
-                       '_source' => $source,
-                       '_post_author' => $author
-                     ),
-                  );
+                        'post_author' => $user_id,
+                        'post_content' => $body,
+                        'post_title' => $title,
+                        'post_status' => "publish",
+                        'post_type' => "post",
+                        'guid' => $id,
+                        'post_category' => array(
+                            $category->term_id
+                        ) ,
+                        'meta_input' => array(
+                            '_source' => $source,
+                            '_post_author' => $author
+                        ) ,
+                    );
                     if ($post_id = wp_insert_post($args)) {
-                      wp_set_post_tags( $post_id, $tags, true );
+                        wp_set_post_tags($post_id, $tags, true); //add tags to post
                     }
                 }
             }
@@ -178,37 +216,42 @@ function get_posts_from_feed( )
     }
 }
 
-
-add_filter( 'cron_schedules', 'isa_add_every_three_minutes' );
-function isa_add_every_three_minutes( $schedules )
+/**
+* Sets custom cron timing
+* @param wp built in object
+*/
+function isa_add_every_three_minutes($schedules)
 {
-    $schedules[ 'every_three_minutes' ] = array(
-         'interval' => 600,
-        'display' => __( 'Every 3 Minutes', 'textdomain' )
+    $schedules['every_three_minutes'] = array(
+        'interval' => 600,
+        'display' => __('Every 3 Minutes', 'textdomain')
     );
     return $schedules;
 }
 
-// Schedule an action if it's not already scheduled
-if ( !wp_next_scheduled( 'isa_add_every_three_minutes' ) ) {
-    wp_schedule_event( time(), 'every_three_minutes', 'isa_add_every_three_minutes' );
-}
-
-// Hook into that action that'll fire every three minutes
-add_action( 'isa_add_every_three_minutes', 'get_posts_from_feed' );
-
-add_action( 'save_post', 'save_original_post', 10 );
-
-function save_original_post( $post )
+/**
+* Sets meta _source to internal if created from GUI (posts not posted from get_posts_from_feed)
+* @param int $post id of the post
+*/
+function save_original_post($post)
 {
     global $wpdb;
-    $query   = $wpdb->prepare( 'SELECT wp_posts.id
+    $query = $wpdb->prepare('SELECT wp_posts.id
     FROM wp_posts
     JOIN wp_postmeta
       ON wp_postmeta.post_id = %d
-      WHERE wp_postmeta.meta_key = "_source";', $post );
-    $results = $wpdb->get_results( $query );
-    if ( !$results ) {
-        add_post_meta( $post, '_source', 'internal' );
+      WHERE wp_postmeta.meta_key = "_source";', $post);
+    $results = $wpdb->get_results($query);
+    if (!$results) {
+        add_post_meta($post, '_source', 'internal');
     }
 }
+
+if (!wp_next_scheduled('isa_add_every_three_minutes')) {
+  wp_schedule_event(time(), 'every_three_minutes', 'isa_add_every_three_minutes');
+}
+add_action('isa_add_every_three_minutes', 'get_posts_from_feed');
+add_action('save_post', 'save_original_post', 10);
+add_action('init', 'rssfeeder_install');
+add_filter('cron_schedules', 'isa_add_every_three_minutes');
+register_activation_hook(__FILE__, 'rssfeeder_install');
