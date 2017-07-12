a911588e730c9d79bd52a59da8ba156b861b9a0a
jdc <jdcxxx@gmail.com>
2017-07-11 23:25:53 -0700

    Remove wordpress site so plugin is only the plugin is visible in repo

--- wp-content/themes/rocketml-wp/template-parts/content-blog.php
+++
@@ -1,9 +0,0 @@
-<!-- blog page welcome section -->
-<section class="container text-center welcome-message">
-  <div class="row">
-    <div class="col-md-12">
-      <h1>RocketML Blog</h1>
-      <h2>Fastest Computational Engine for Machine Learning </h2>
-    </div>
-  </div>
-</section>
